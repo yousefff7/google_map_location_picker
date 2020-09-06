957c15e7e1c92e687b5d3183baf3756d94741686
yousefff7 <70837900+yousefff7@users.noreply.github.com>
2020-09-06 18:01:36 +0300

    Update map.dart

--- lib/src/map.dart
+++ lib/src/map.dart
@@ -264,7 +264,7 @@ class MapPickerState extends State<MapPicker> {
                     onPressed: () {
                                     Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
                                                         
-                return widget.navigatePage( address: _address);
+                return widget.navigatePage();
               }));       
 //                       Navigator.of(context).pop({
 //                         'location': LocationResult(
